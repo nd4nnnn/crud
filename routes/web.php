68f66b45808aafcfe4e9827a3b72456d75b22b7e
hamdan <nd4nnnn02@gmail.com>
2026-04-20 08:55:06 +0700

    up

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SiswaController;
 use App\Http\Controllers\KelasController;
+use App\Http\Controllers\AuthController;
 
 Route::resource('siswa', SiswaController::class);
 Route::resource('kelas', KelasController::class);
+
+
+Route::get('/login', [AuthController::class, 'loginForm'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
